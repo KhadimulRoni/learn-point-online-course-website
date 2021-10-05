8cca51308bc9d16d8b7d70fdc07107aa8a4fca99
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-10-05 21:54:33 +0600

    backround images changed

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -6,9 +6,9 @@ const About = () => {
     return (
         <div className="aboutUs">
             <h1 className="aboutTitle">About Us</h1>
-            <div className="logo aboutText">
+            <div className="logo2">
                         <img src={image} alt="" />
-                        <h4 className="p-5">Our academy offers a wide range of stationery and online courses. we help our students to get the best education and give a start to their careers! This is One of the world's best-known online learning resources, Coursera has several thousand courses from leading educational providers.An online class is a course conducted over the Internet. They are generally conducted through a learning management system, in which students can view their course syllabus and academic progress, as well as communicate with fellow students and their course instructor.</h4>
+                        <h4 className="aboutText p-5">Our academy offers a wide range of stationery and online courses. we help our students to get the best education and give a start to their careers! This is One of the world's best-known online learning resources, Coursera has several thousand courses from leading educational providers.An online class is a course conducted over the Internet.</h4>
             </div>
         </div>
     );
--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import { Col, FloatingLabel, Form, Row } from 'react-bootstrap';
 import './ContactUs.css'
 
 const ContactUs = () => {
     return (
         <div className="contactUs">
-            <div className="about p-5">
+            <div className="contact p-5">
                         <h2 className="contactTitle">Contact Us</h2>
-                        <h3>Call Now</h3>
-                        <h3>0 (800) 516 90 58</h3>
+                        <h3 className="pt-5">Call Now</h3>
+                        <h5 className="text-warning">0 (800) 516 90 58</h5>
+                        <h3>Email</h3>
+                        <h5 className="text-warning">learnpoint777@gmail.com</h5>
                         <h3>Visit Learn Point</h3>
-                        <h3 className="text-danger">5500, Edisson Street, New York, NY, 88030</h3>
+                        <h5 className="text-warning">5500, Edisson Street, New York, NY, 88030</h5>
+            </div>
+            <div className="input">
+                <div>
+                    <Form>
+                        <Row>
+                            <Col>
+                            <Form.Control placeholder="Your Name" />
+                            </Col>
+                            <Col>
+                            <Form.Control placeholder="Email" />
+                            </Col>
+                        </Row>
+                    </Form>
+                </div>
+                <div className="pt-4">
+                    <Form>
+                        <Form.Control placeholder="Subject" />
+                    </Form>
+                </div>
+                <div className="pt-4">
+                    <>
+                        <FloatingLabel controlId="floatingTextarea2" label="Your Texts">
+                            <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}
+                            />
+                        </FloatingLabel>
+                    </>
+                </div>
             </div>
         </div>
     );
